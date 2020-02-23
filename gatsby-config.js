80d8782daa6b63a63b8885e4c4771b210634bf57
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-02-23 02:08:56 +0200

    optimize fonts & images

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,8 @@ module.exports = {
           {
             family: `Roboto`,
             variants: [`100`, `300`, `300italic`],
+            formats: ['woff2'],
+            fontDisplay: 'swap',
           },
         ],
       },
