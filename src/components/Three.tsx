4416221740c722ed424e162bab5d1b9b7d071b9e
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-03-08 19:01:21 +0200

    update text in landing page

--- src/components/Three.tsx
+++ src/components/Three.tsx
@@ -2,48 +2,47 @@ import React from 'react';
 import pic04 from '../assets/images/pic04.jpg';
 import { Link as ScrollLink } from 'react-scroll';
 import Fade from 'react-reveal/Fade';
+import { useTranslation } from 'react-i18next';
 
-const Three: React.FC = () => (
-  <section
-    id="three"
-    className="spotlight style3 left inactive"
-    style={{ backgroundImage: `url(${pic04})` }}
-  >
-    <span className="image fit main bottom">
-      <img src={pic04} alt="" />
-    </span>
-    <Fade left big>
-      <div className="content">
-        <header>
-          <h2>Interdum felis blandit praesent sed augue</h2>
-          <p>Accumsan integer ultricies aliquam vel massa sapien phasellus</p>
-        </header>
-        <p>
-          Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent
-          adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor
-          lorem et accumsan consequat adipiscing lorem.
-        </p>
-        <ul className="actions">
-          <li>
-            <a href="/" className="button">
-              Learn More
-            </a>
-          </li>
-        </ul>
-      </div>
-    </Fade>
-    <ScrollLink
-      to="four"
-      className="goto-next"
-      activeClass="active"
-      smooth={true}
-      offset={50}
-      duration={1500}
-      spy={true}
+const Three: React.FC = () => {
+  const { t } = useTranslation();
+  return (
+    <section
+      id="three"
+      className="spotlight style3 left inactive"
+      style={{ backgroundImage: `url(${pic04})` }}
     >
-      Next
-    </ScrollLink>
-  </section>
-);
+      <span className="image fit main bottom">
+        <img src={pic04} alt="" />
+      </span>
+      <Fade left big>
+        <div className="content">
+          <header>
+            <h2>{t('landing.missions.three.title')}</h2>
+          </header>
+          <p>{t('landing.missions.three.details')}</p>
+          <ul className="actions">
+            <li>
+              <a href="/" className="button">
+                {t('common.learnMore')}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </Fade>
+      <ScrollLink
+        to="four"
+        className="goto-next"
+        activeClass="active"
+        smooth={true}
+        offset={50}
+        duration={1500}
+        spy={true}
+      >
+        {t('common.next')}
+      </ScrollLink>
+    </section>
+  );
+};
 
 export default Three;
--- src/components/Two.tsx
+++ src/components/Two.tsx
@@ -2,48 +2,47 @@ import React from 'react';
 import pic03 from '../assets/images/pic03.jpg';
 import { Link as ScrollLink } from 'react-scroll';
 import Fade from 'react-reveal/Fade';
+import { useTranslation } from 'react-i18next';
 
-const Two: React.FC = () => (
-  <section
-    id="two"
-    className="spotlight style2 right inactive"
-    style={{ backgroundImage: `url(${pic03})` }}
-  >
-    <span className="image fit main">
-      <img src={pic03} alt="" />
-    </span>
-    <Fade right big>
-      <div className="content">
-        <header>
-          <h2>Interdum amet non magna accumsan</h2>
-          <p>Nunc commodo accumsan eget id nisi eu col volutpat magna</p>
-        </header>
-        <p>
-          Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent
-          adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor
-          lorem et accumsan consequat adipiscing lorem.
-        </p>
-        <ul className="actions">
-          <li>
-            <a href="/" className="button">
-              Learn More
-            </a>
-          </li>
-        </ul>
-      </div>
-    </Fade>
-    <ScrollLink
-      to="three"
-      className="goto-next"
-      activeClass="active"
-      smooth={true}
-      offset={50}
-      duration={1500}
-      spy={true}
+const Two: React.FC = () => {
+  const { t } = useTranslation();
+  return (
+    <section
+      id="two"
+      className="spotlight style2 right inactive"
+      style={{ backgroundImage: `url(${pic03})` }}
     >
-      Next
-    </ScrollLink>
-  </section>
-);
+      <span className="image fit main">
+        <img src={pic03} alt="" />
+      </span>
+      <Fade right big>
+        <div className="content">
+          <header>
+            <h2>{t('landing.missions.two.title')}</h2>
+          </header>
+          <p>{t('landing.missions.two.details')}</p>
+          <ul className="actions">
+            <li>
+              <a href="/" className="button">
+                {t('common.learnMore')}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </Fade>
+      <ScrollLink
+        to="three"
+        className="goto-next"
+        activeClass="active"
+        smooth={true}
+        offset={50}
+        duration={1500}
+        spy={true}
+      >
+        {t('common.next')}
+      </ScrollLink>
+    </section>
+  );
+};
 
 export default Two;
