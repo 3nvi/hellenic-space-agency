5ac6025070b28603fdb135ef5fde236e35613c3c
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-03-08 19:00:47 +0200

    remove social links from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,26 +2,9 @@ import React from 'react';
 
 const Footer: React.FC = () => (
   <footer id="footer">
-    <ul className="icons">
-      <li>
-        <a href="/" className="icon brands alt fa-twitter">
-          <span className="label">Twitter</span>
-        </a>
-      </li>
-      <li>
-        <a href="/" className="icon brands alt fa-instagram">
-          <span className="label">Instagram</span>
-        </a>
-      </li>
-      <li>
-        <a href="/" className="icon solid alt fa-envelope">
-          <span className="label">Email</span>
-        </a>
-      </li>
-    </ul>
     <ul className="copyright">
       <li>&copy; All rights reserved.</li>
-      <li>Hellenic Space Agency</li>
+      <li>Hellenic Space Center</li>
     </ul>
   </footer>
 );
