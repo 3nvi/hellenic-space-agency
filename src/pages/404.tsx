f3c00b704e47aa84cdf20418815a55fc68d11e86
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-03-09 23:31:20 +0200

    add not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import { useTranslation } from 'react-i18next';
 
-const NotFoundPage: React.FC = () => (
-  <Layout>
-    <SEO title="404" description="The page you requested could not be found" />
-    <main className="wrapper style1">
-      <section id="one">
-        <div className="inner">
-          <h1>NOT FOUND</h1>
-          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        </div>
-      </section>
-    </main>
-  </Layout>
-);
+const NotFoundPage: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Layout>
+      <SEO title="404" />
+      <main className="wrapper style1">
+        <section className="container" style={{ padding: '10em' }}>
+          <header className="major">
+            <h2>{t('404.mainTitle')}</h2>
+            <p>{t('404.mainSubtitle')}</p>
+          </header>
+        </section>
+      </main>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
