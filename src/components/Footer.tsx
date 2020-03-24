bd3323f6a976a0e938acba77e0a57b223934767f
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-03-24 21:51:15 +0200

    update banner images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,16 @@ const Footer: React.FC = () => (
     <ul className="copyright">
       <li>&copy; All rights reserved.</li>
       <li>Hellenic Space Center</li>
+      <li>
+        Photos by{' '}
+        <a href="https://www.nasa.gov/" target="_blank" rel="noopener noreferrer">
+          Nasa
+        </a>
+        ,{' '}
+        <a href="https://imgur.com/rzMZRFt" target="_blank" rel="noopener noreferrer">
+          Inefect69
+        </a>
+      </li>
     </ul>
   </footer>
 );
