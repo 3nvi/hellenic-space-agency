ab58ab4f4269267c52b6b732b05e1aebb06fb102
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-03-25 18:44:33 +0200

    add photo credentials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,29 @@ const Footer: React.FC = () => (
       <li>&copy; All rights reserved.</li>
       <li>Hellenic Space Center</li>
       <li>
-        Photos by{' '}
+        Photos from{' '}
         <a href="https://www.nasa.gov/" target="_blank" rel="noopener noreferrer">
           Nasa
         </a>
         ,{' '}
         <a href="https://imgur.com/rzMZRFt" target="_blank" rel="noopener noreferrer">
           Inefect69
+        </a>
+        ,{' '}
+        <a
+          href="https://pixabay.com/images/search/planet/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pixabay
+        </a>{' '}
+        and{' '}
+        <a
+          href="https://www.inverse.com/article/44289-pink-moon-surprise-venus-mars-jupiter-saturn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Inverse
         </a>
       </li>
     </ul>
