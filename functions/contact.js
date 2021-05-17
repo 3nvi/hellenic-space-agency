0f5a03bb66496ad66894e9417cfe6c8f395ee4d0
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2021-05-17 17:22:19 +0300

    feat: attempt to use oauth2 for email sending

--- functions/contact.js
+++ functions/contact.js
@@ -1,18 +1,32 @@
 const nodemailer = require('nodemailer');
+const { OAuth2 } = require('googleapis');
+
+const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground';
 
 exports.handler = function(event, context, callback) {
   if (event.httpMethod !== 'POST') {
     return callback(null, { statusCode: 405, body: 'Method Not Allowed' });
   }
 
-  // create a transporter
+  const oauth2Client = new OAuth2(
+    process.env.MAILING_SERVICE_CLIENT_ID,
+    process.env.MAILING_SERVICE_CLIENT_SECRET,
+    OAUTH_PLAYGROUND
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.MAILING_SERVICE_REFRESH_TOKEN,
+  });
+
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: 465,
-    secure: true,
+    service: 'gmail',
     auth: {
+      type: 'OAuth2',
       user: process.env.CONTACT_EMAIL,
-      pass: process.env.CONTACT_PASSWORD,
+      clientId: process.env.MAILING_SERVICE_CLIENT_ID,
+      clientSecret: process.env.MAILING_SERVICE_CLIENT_SECRET,
+      refreshToken: process.env.MAILING_SERVICE_REFRESH_TOKEN,
+      accessToken: oauth2Client.getAccessToken(),
     },
   });
 
